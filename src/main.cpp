a2945e8c749ac4c074955e619ffb665b1d21ae0a
Anoof Chappangathil <anoof7262@gmail.com>
2024-09-10 17:55:12 +0400

    Refactor main.cpp to improve readability and maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,36 @@
-#define DEBUG     0         // 0 = off, 1 = on
+#define DEBUG     0         // 0 = DEBUG OFF, 1 = DEBUG ON
 #define RST_PIN   D0        
 #define SS_PIN    D8  
 
 #define TAG1 "13BD161D"     
-#define TAG2 "231CAFE"
+#define TAG2 "53C38BE"
 #define TAG3 "A348261B"
 #define TAG4 "39BF8F7"
 #define TAG5 "931B21D"
 #define TAG6 "338221B"
- 
+
+#include <Arduino.h> 
 #include <SPI.h>
 #include <MFRC522.h>
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
-String tagID = "";
+String tagID = "";                  // String to store the tag ID
 
-MFRC522::Uid id;
-uint8_t control = 0x00;
+MFRC522::Uid id;                    // Create a struct to store the ID of the card
+uint8_t control = 0x00;             // Variable to store the control value
 
 //*****************************************************************************************//
 //                                      FUNCTIONS                                        //
+/**
+ * Checks the tag ID and Serial Print corresponding Command.
+ * 
+ * This function takes a tag ID as input and checks it against predefined tag IDs (TAG1, TAG2, TAG3, TAG4, TAG5, TAG6). 
+ * If the tag ID matches any of the predefined tags, it prints a corresponding Command to the serial monitor. 
+ * If the tag ID does not match any of the predefined tags and the DEBUG flag is set, it prints an "Unknown Tag" message to the serial monitor.
+ *
+ * @param tag_ID The tag ID to be checked.
+ */
 void checkTag(String tag_ID){
   if      (tag_ID == TAG1) { Serial.println("*A#"); }
   else if (tag_ID == TAG2) { Serial.println("*B#"); }
@@ -31,6 +41,16 @@ void checkTag(String tag_ID){
   else    { if (DEBUG){Serial.println("Unknown Tag");} }
 }
 
+/**
+ * Stores the tag ID by converting the given 8-bit data into a hexadecimal string with leading zeroes.
+ * 
+ * This function takes an array of 8-bit data and its length as parameters. 
+ * It then converts each byte of the data into a hexadecimal string and concatenates them to form the tag ID. 
+ * The resulting tag ID is stored in the global variable 'tagID'.
+ * 
+ * @param data Pointer to the array of 8-bit data.
+ * @param length Length of the data array.
+ */
 void storeTag(uint8_t *data, uint8_t length) // prints 8-bit data in hex with leading zeroes
 {
   tagID = "";
@@ -41,6 +61,15 @@ void storeTag(uint8_t *data, uint8_t length) // prints 8-bit data in hex with le
  
 
  
+/**
+ * Copies the UID and related information from the `mfrc522` object to the provided `id` object.
+ * 
+ * This function initializes the `id` object by setting all its bytes to zero using `memset`. 
+ * Then, it copies the UID bytes from the `mfrc522` object to the `id` object using `memcpy`. 
+ * It also sets the `size` and `sak` properties of the `id` object to the corresponding values from the `mfrc522` object.
+ * 
+ * id A pointer to the `MFRC522::Uid` object where the UID and related information will be copied.
+ */
 void cpid(MFRC522::Uid *id){
   memset(id, 0, sizeof(MFRC522::Uid));
   memcpy(id->uidByte, mfrc522.uid.uidByte, mfrc522.uid.size);
@@ -48,6 +77,15 @@ void cpid(MFRC522::Uid *id){
   id->sak = mfrc522.uid.sak;
 }
 
+/**
+ * Reads an RFID card and processes its ID.
+ *
+ * This function checks for the presence of a new RFID card and reads its serial number.
+ * It then processes the card ID by converting it to uppercase, storing it, and checking it against known tags.
+ * If a known tag is detected, a specific message is printed. The function also handles the card removal process.
+ *
+ *  The function includes debug print statements if the DEBUG flag is set.
+ */
 void readCard(){
   if (!mfrc522.PICC_IsNewCardPresent()) { return; }
   if (!mfrc522.PICC_ReadCardSerial()) { return; }
@@ -78,6 +116,14 @@ void readCard(){
   mfrc522.PCD_StopCrypto1();
 }
 
+/**
+ * Initializes the serial communication, SPI bus, and RFID reader.
+ * 
+ * This function sets up the necessary components for the RFID reader to function.
+ * It initializes the serial communication at a baud rate of 115200 for debugging purposes,
+ * starts the SPI bus, and initializes the RFID reader. After a short delay, it prints
+ * the current antenna gain setting of the RFID reader to the serial monitor.
+ */
 void setup(){
   Serial.begin(115200);            // Initialize serial communications with the PC
   SPI.begin();                     // Init SPI bus
@@ -87,8 +133,8 @@ void setup(){
   
   delay(50);
   
-  Serial.print("Antenna Gain: ");
-  Serial.println(mfrc522.PCD_GetAntennaGain(), HEX);
+  // Serial.print("Antenna Gain: ");
+  // Serial.println(mfrc522.PCD_GetAntennaGain(), HEX);
 }
 
 void loop() {
